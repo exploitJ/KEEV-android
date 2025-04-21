a45c9d955d5a3ec27817ea15aaf5aeba43abdf75
Serim Son <serim711@gmail.com>
2025-04-21 16:12:36 +0900

    feat: gradle managed devices settings

---
+++ build-logic/convention/src/main/kotlin/com/hmhz/keev/GradleManagedDevices.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package com.hmhz.keev
+
+import com.android.build.api.dsl.CommonExtension
+import com.android.build.api.dsl.ManagedVirtualDevice
+import org.gradle.kotlin.dsl.get
+import org.gradle.kotlin.dsl.invoke
+
+/**
+ * Configure project for Gradle managed devices
+ */
+internal fun configureGradleManagedDevices(
+    commonExtension: CommonExtension<*, *, *, *, *, *>,
+) {
+    val pixel4 = DeviceConfig("Pixel 4", 30, "aosp-atd")
+    val pixel9 = DeviceConfig("Pixel 9 Pro XL", 36, "google")
+    val pixelTablet = DeviceConfig("Pixel Tablet", 33, "aosp-atd")
+
+    val deviceList = listOf(pixel4, pixel9, pixelTablet)
+    val ciDevices = listOf(pixel4, pixelTablet)
+
+    commonExtension.testOptions {
+        managedDevices {
+            localDevices {
+                deviceList.forEach { deviceConfig ->
+                    maybeCreate(deviceConfig.taskName, ManagedVirtualDevice::class.java).apply {
+                        device = deviceConfig.device
+                        apiLevel = deviceConfig.apiLevel
+                        systemImageSource = deviceConfig.systemImageSource
+                    }
+                }
+            }
+            groups {
+                maybeCreate("ci").apply {
+                    ciDevices.forEach { deviceConfig ->
+                        targetDevices.add(allDevices[deviceConfig.taskName])
+                    }
+                }
+            }
+        }
+    }
+}
+
+private data class DeviceConfig(
+    val device: String,
+    val apiLevel: Int,
+    val systemImageSource: String,
+) {
+    val taskName = buildString {
+        append(device.lowercase().replace(" ", ""))
+        append("api")
+        append(apiLevel.toString())
+        append(systemImageSource.replace("-", ""))
+    }
+}
