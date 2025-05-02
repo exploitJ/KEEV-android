71404df0981070f7c0b02402bd986f233074c1d4
Serim Son <serim711@gmail.com>
2025-05-03 03:54:51 +0900

    fix: set default theme to dark

--- core/designsystem/src/main/kotlin/com/hmhz/keev/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/kotlin/com/hmhz/keev/core/designsystem/theme/Theme.kt
@@ -1,40 +1,41 @@
 package com.hmhz.keev.core.designsystem.theme
 
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.*
 
-private val LightColorScheme = lightColorScheme(
+private val DarkColorScheme = darkColorScheme(
     primary = BrandColor500,
     onPrimary = Gray50,
-    primaryContainer = BrandColor50,
-    onPrimaryContainer = BrandColor500,
+    primaryContainer = BrandColor500a20,
+    onPrimaryContainer = BrandColor50,
     secondary = BrandColor500a50,
     onSecondary = Gray50,
-    secondaryContainer = BrandColor50,
-    onSecondaryContainer = BrandColor500,
-    tertiary = BrandColor500a20,
+    secondaryContainer = BrandColor500a20,
+    onSecondaryContainer = BrandColor50,
+    tertiary = BrandColor500a50,
     onTertiary = Gray50,
-    tertiaryContainer = BrandColor50,
-    onTertiaryContainer = BrandColor500,
-    background = Gray50,
-    onBackground = Gray900,
-    surface = Gray50,
-    onSurface = Gray900,
-    surfaceVariant = Gray100,
-    onSurfaceVariant = Gray700,
+    tertiaryContainer = BrandColor500a20,
+    onTertiaryContainer = BrandColor50,
+    background = Gray900,
+    onBackground = Gray50,
+    surface = Gray900,
+    onSurface = Gray50,
+    surfaceVariant = Gray800,
+    onSurfaceVariant = Gray300,
     surfaceTint = BrandColor500,
-    inverseSurface = Gray900,
-    inverseOnSurface = Gray50,
+    inverseSurface = Gray50,
+    inverseOnSurface = Gray900,
     error = Error,
     onError = Gray50,
-    errorContainer = Error.copy(alpha = 0.1f),
+    errorContainer = Error.copy(alpha = 0.2f),
     onErrorContainer = Error,
-    outline = Gray300,
-    outlineVariant = Gray100,
-    scrim = Gray900.copy(alpha = 0.3f),
+    outline = Gray700,
+    outlineVariant = Gray800,
+    scrim = Gray900.copy(alpha = 0.5f)
 )
-
 
 @Composable
 fun KEEVTheme(
@@ -43,15 +44,14 @@ fun KEEVTheme(
 //    dynamicColor: Boolean = true,
     content: @Composable () -> Unit,
 ) {
-    val colorScheme = when {
+    val colorScheme = darkColorScheme()
 //        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
 //            val context = LocalContext.current
 //            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
 //        }
 //
 //        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+
 
     MaterialTheme(
         colorScheme = colorScheme,
